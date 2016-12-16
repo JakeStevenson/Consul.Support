74cc4b7c9eac5cd607bdc715b0b8c356e5b7bb3e
Jake A. Stevenson <jake.stevenson@gmail.com>
2016-12-16 07:54:02 -0600

    Convert WebAPI over

---
+++ examples/WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebAPI.Configuration;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly ValuesControllerSettings _options;
+        public ValuesController(IOptions<ValuesControllerSettings> options)
+        {
+            _options = options.Value;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[]
+            {
+                _options.Test1,
+                _options.Test2
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
